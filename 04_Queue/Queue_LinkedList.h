a8470458f2fbbae7ea5e5c559abcf6e704a0f31e
AminNavabzade <aminnavabzade@gmail.com>
2026-09-20 00:47:10 +0330

    add queue whit linked list part 2

--- 04_Queue/Queue_LinkedList.h
+++ 04_Queue/Queue_LinkedList.h
@@ -35,3 +35,42 @@ void Queue_L::enqueue(int ele){
     }
     count++;
 }
+int Queue_L::dequeue(){
+    if(isEmpty()){
+        return -1;
+    }
+    Node *temp = front;
+    int ele = temp->data;
+    front = front->next;
+    if(front == nullptr){
+        rear = nullptr;
+    }
+    delete temp;
+    count--;
+    return ele;
+}
+int Queue_L::peek(){
+    if(isEmpty()){
+        return -1;
+    }
+    return front->data;
+}
+void Queue_L::display(){
+    if(isEmpty()){
+        return;
+    }
+    Node *temp = front;
+    while(temp != nullptr){
+        cout<<temp->data<<" , ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+void Queue_L::revers(){
+    if(isEmpty()){
+        return;
+    }
+    int frontEle = dequeue();
+    revers();
+    enqueue(frontEle);
+}
